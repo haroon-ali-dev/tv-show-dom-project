26c42a2e31c174275e20ab82a87c9c2b3c6ad2cd
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-10-29 22:32:26 +0100

    add search feature and syle it

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-window.onload = () => {
-  const containerEpisodes = document.querySelector(".container-episodes");
-  const episodes = getAllEpisodes();
+let eps;
 
-  for (let episode of episodes) {
+window.onload = () => {
+  eps = getAllEpisodes();
+
+  displayEps(eps);
+}
+
+function displayEps(eps) {
+  const containerEps = document.querySelector(".container-eps");
+  containerEps.innerHTML = "";
+
+  for (let ep of eps) {
     const article = document.createElement("article");
 
     const epTitleCont = document.createElement("div");
     epTitleCont.setAttribute("class", "cont-ep-title");
     const epTitle = document.createElement("h2");
-    epTitle.innerText = `${episode.name} - S${zeroPad(episode.season)}E${zeroPad(episode.number)}`;
+    epTitle.innerText = `${ep.name} - S${zeroPad(ep.season)}E${zeroPad(ep.number)}`;
     epTitleCont.appendChild(epTitle);
 
     const epImg = document.createElement("img");
     epImg.setAttribute("class", "ep-img");
-    epImg.setAttribute("src", episode.image.medium);
+    epImg.setAttribute("src", ep.image.medium);
+    epImg.setAttribute("alt", ep.name);
 
     article.appendChild(epTitleCont);
     article.appendChild(epImg);
-    article.insertAdjacentHTML("beforeend", episode.summary);
+    article.insertAdjacentHTML("beforeend", ep.summary);
 
-    containerEpisodes.appendChild(article);
+    containerEps.appendChild(article);
   }
 }
+
+document.querySelector("#search").addEventListener("keyup", (e) => {
+  const query = e.target.value;
+  const searchText = document.querySelector("#search-text");
+
+  epsFiltered = eps.filter(ep => ep.name.toLowerCase().includes(query.toLowerCase()) || ep.summary.toLowerCase().includes(query.toLowerCase()));
+
+  if (query) {
+    searchText.innerText = `Displaying ${epsFiltered.length}/${eps.length} episodes.`;
+  } else {
+    searchText.innerText = "";
+  }
+
+
+  displayEps(epsFiltered);
+});
 
 function zeroPad(num) {
   return num < 10 ? `0${num}` : num;
